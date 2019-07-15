7e7f96488ba8a4f31566b4c3c1b9027dcba238ce
Farshad Basereh <49536490+Farshad-94@users.noreply.github.com>
2019-07-15 14:26:21 -0400

    update ports, add procfile

--- app.js
+++ app.js
@@ -130,7 +130,10 @@ app.post("/", function(req, res) {
   });
 });
 
-
-app.listen(3000, function(req, res) {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function(req, res) {
+  console.log("Server has started successfully");
 });
